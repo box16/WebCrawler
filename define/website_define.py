f83ba34ad670f87abdc7f5fc622bd1386add6679
box16 <inoinobox16@gmail.com>
2020-11-23 16:30:36 +0900

    web情報のモジュール化

---
+++ define/website_define.py
@@ -0,0 +1,17 @@
+import re
+
+web_sites = [{"name": "LifeHacker",
+              "domain": "https://www.lifehacker.jp/",
+              "title_tag": "[class='lh-entryDetail-header'] h1",
+              "body_tag": "[id='realEntryBody'] p",
+              "dome_char": re.compile("^(/20)"),
+              "dome_page": lambda domestic_url: "https://www.lifehacker.jp/" + domestic_url,
+            },
+            { "name": "PaleolithicMan",
+              "domain": "https://yuchrszk.blogspot.com/",
+              "title_tag": "[class='post-title single-title emfont']",
+              "body_tag": "[class='post-single-body post-body'] p",
+              "dome_char": re.compile("""^(?=https://yuchrszk.blogspot.com/..../.+?)(?!.*archive)(?!.*label).*$"""),
+              "dome_page": lambda domestic_url: domestic_url,
+            },
+            ]
--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-from mylib import webcraw
-import re
+from mylib  import webcraw
+from define import website_define
 
 if __name__ == "__main__":
-    web_sites = [{"name": "LifeHacker",
-                  "domain": "https://www.lifehacker.jp/",
-                  "title_tag": "[class='lh-entryDetail-header'] h1",
-                  "body_tag": "[id='realEntryBody'] p",
-                  "dome_char": re.compile("^(/20)"),
-                  "dome_page": lambda domestic_url: "https://www.lifehacker.jp/" + domestic_url,
-                  },
-                 {"name": "PaleolithicMan",
-                  "domain": "https://yuchrszk.blogspot.com/",
-                  "title_tag": "[class='post-title single-title emfont']",
-                  "body_tag": "[class='post-single-body post-body'] p",
-                  "dome_char": re.compile("""^(?=https://yuchrszk.blogspot.com/..../.+?)(?!.*archive)(?!.*label).*$"""),
-                  "dome_page": lambda domestic_url: domestic_url,
-                  },
-                 ]
-
+    web_sites = website_define.web_sites
     for site in web_sites:
         _website = webcraw.Website(**site)
         webcraw.Crawler().start_craw(_website, 10)
