add84ba04f55e42ca0605a273f36cbdfe71c7617
box16 <inoinobox16@gmail.com>
2020-11-25 19:42:05 +0900

    Crawlerの役割をUMLに対応

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from define import website_define
 
 if __name__ == "__main__":
     web_sites = website_define.web_sites
+    crawler = webcraw.Crawler()
     for site in web_sites:
         _website = webcraw.Website(**site)
-        crawler = webcraw.Crawler(_website)
-        crawler.start_craw()
-        crawler.write_contents()
-        del(crawler)
+        crawler.start_craw(_website)
