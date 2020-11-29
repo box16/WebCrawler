60c109f31285573f4bf254585a66a146964f8a6a
box16 <inoinobox16@gmail.com>
2020-11-29 09:24:22 +0900

    リファクタ

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from mymod import webcraw
-from mymod import db_access
 from mymod import d2v
 from define import website_define
-from time import sleep
 
 if __name__ == "__main__":
     web_sites = website_define.web_sites
     crawler = webcraw.Crawler()
     for site in web_sites:
         _website = webcraw.Website(**site)
-        crawler.start_craw(_website, 1)
+        crawler.start_craw(_website, 50)
     machine = d2v.D2V()
     machine.training()
-    #db = db_access.DBAccess()
-    # db.update_keyword()
--- outputfile.py
+++
@@ -1,5 +0,0 @@
-from mymod import db_access
-
-if __name__ == "__main__":
-    db = db_access.DBAccess()
-    db.write_pages_SBJson()
